2d3f5330d1d007b52b184640b7aa747d4c633674
SarvagyaBhasin <63590101+SarvagyaBhasin@users.noreply.github.com>
2020-07-20 17:54:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var flag ="down" ;
+var form, player, game;
+var car1image,car2image,car3image,car4image,track,ground;
+var cars, car1, car2, car3, car4;
+function preload(){
+  track=loadImage("images/track.jpg");
+  car1image=loadImage("images/car1.png");
+  car2image=loadImage("images/car2.png");
+  car3image=loadImage("images/car3.png");
+  car4image=loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4 && flag == "down"){
+    game.update(1);
+    flag = "up";
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState==2){
+    game.end();
+  }
+}
